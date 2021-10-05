77087eae4a82b9abbaf47b819496b08ce8e72884
Ahmed Nabil <a7med.nabil1655@gmail.com>
2021-10-04 17:04:45 -0700

    add all functionality

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -1,57 +1,133 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Define Global Variables
- * 
-*/
-
+ *
+ */
+const activeClass = "active";
+let sectionName, sectionContent;
+let sectionsOffsets = [];
+let navbarList = document.getElementById("navbar__list");
+let main = document.querySelector("main");
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
+ *
+ */
 
+function capitalize(text) {
+  text = text[0].toUpperCase() + text.slice(1);
 
+  return text;
+}
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
+function addSection(sectionName, sectionContent) {
+  navbarList.innerHTML += `
+    <a class="menu__link" href="#${sectionName}">
+      ${capitalize(sectionName)}
+    </a>
+  `;
 
+  main.innerHTML += `
+    <section id="${sectionName}" class="" data-nav="${sectionName}">
+      <div class="landing__container">
+        <h2>${capitalize(sectionName)}</h2>
+        ${sectionContent}
+      </div>
+    </section>
+  `;
+
+  sectionsOffsets.push({
+    id: sectionName,
+    offset: document.getElementById(sectionName).offsetTop,
+  });
+
+  document.querySelectorAll(".menu__link").forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      scrollToSection(
+        document.getElementById(link.getAttribute("href").slice(1))
+      );
+    });
+  });
+}
+
+function setActive(element) {
+  [...element.parentElement.children].forEach((el) =>
+    el.classList.remove(activeClass)
+  );
+  element.classList.add(activeClass);
+}
 
 // Add class 'active' to section when near top of viewport
-
+function isInViewport(
+  currElement = sectionsOffsets[0],
+  nextElement = document.body.offsetHeight
+) {
+  if (
+    window.scrollY >= currElement.offset &&
+    window.scrollY < nextElement.offset
+  ) {
+    setActive(document.getElementById(currElement.id));
+  }
+}
 
 // Scroll to anchor ID using scrollTO event
 
+function scrollToSection(element) {
+  window.scrollTo({
+    top: element.offsetTop,
+    left: 0,
+    behavior: "smooth",
+  });
+}
 
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
+// Build menu
 
-// Scroll to section on link click
+(function addSections(num = 4) {
+  for (let i = 1; i <= num; i++) {
+    console.log(i);
+    addSection(
+      `section${i}`,
+      `<p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Libero asperiores ipsam quidem deleniti error quo, alias ut quod quaerat? Suscipit adipisci sunt reprehenderit hic delectus sed architecto illo ducimus impedit?
+      </p>`
+    );
+  }
+})();
 
-// Set sections as active
-
-
+// Set sections as active on scroll
+window.addEventListener("scroll", function (e) {
+  for (let i = 0; i < sectionsOffsets.length; i++) {
+    if (i < sectionsOffsets.length) {
+      isInViewport(sectionsOffsets[i], sectionsOffsets[i + 1]);
+    }
+  }
+});
